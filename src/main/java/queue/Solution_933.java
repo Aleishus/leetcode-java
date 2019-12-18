0552d21ce2a467c957103dce2f8561db03ab5c93
aleishus <941314zx>
2019-12-18 17:22:59 +0800

    993,701

--- src/main/java/queue/Solution_933.java
+++ src/main/java/queue/Solution_933.java
@@ -8,11 +8,29 @@ package queue;
  */
 public class Solution_933 {
 
-    public Solution_933() {
+  int[] queue;
+  int head;
+  int rear;
 
+
+  public Solution_933() {
+    queue = new int[3000];
+    head = 0;
+    rear = -1;
+  }
+
+
+  public int ping(int t) {
+    queue[++rear] = t;
+
+    while (head != rear) {
+      if (queue[rear] - queue[head] > 3000) {
+        head++;
+      } else {
+        break;
+      }
     }
 
-    public int ping( int t ) {
-        return 0;
-    }
+    return rear - head + 1;
+  }
 }
