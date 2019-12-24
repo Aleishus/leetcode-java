61110174d03956da15daa3742542842cf70751bf
zhouxi <fancy9044@gmail.com>
2019-12-25 00:45:35 +0800

    调整结构

--- src/main/java/heap/Solution_703.java
+++ src/main/java/heap/Solution_703.java
@@ -1,54 +1,66 @@
 package heap;
 
+import java.util.Arrays;
+
 public class Solution_703 {
 
-  int k;
-  int[] nums;
-  int m;
 
-  public Solution_703(int k, int[] nums) {
-    this.k = k;
-    this.nums = nums;
-    this.m = nums.length / 2 - 1; // m 表示满二叉树最后一个非叶子节点
-    for (int i = m; i >= 0; i--) {
-      buildMaxHeap(nums, i);
+    int[] heap;
+    int count = 0;
+    int k;
+
+    public Solution_703( int k, int[] nums ) {
+        heap = new int[k];
+        Arrays.fill(heap,Integer.MAX_VALUE);
+        this.k = k;
+        for (int i = 0; i < nums.length; i++) {
+            add(nums[i]);
+        }
     }
 
-  }
 
-
-  private void swap(int[] num, int i, int j) {
-    int tmp = num[i];
-    num[i] = num[j];
-    num[j] = tmp;
-  }
-
-
-  private void buildMaxHeap(int[] nums, int noLeaf) {
-    int left = 2 * noLeaf + 1;
-    int right = left + 1;
-    int big = noLeaf;
-    if (nums[left] > nums[big]) {
-      big = left;
+    private void swap( int[] num, int i, int j ) {
+        int tmp = num[i];
+        num[i] = num[j];
+        num[j] = tmp;
     }
-    if (right < nums.length && nums[right] > nums[big]) {
-      big = right;
+
+    private void buildMinHeap( int[] nums, int noLeaf ) {
+        int left = 2 * noLeaf + 1;
+        int right = left + 1;
+        int lower = noLeaf;
+        if (left < nums.length && nums[left] < nums[lower]) {
+            lower = left;
+        }
+        if (right < nums.length && nums[right] < nums[lower]) {
+            lower = right;
+        }
+        if (lower != noLeaf) {
+            swap(nums, lower, noLeaf);
+            buildMinHeap(nums, lower);
+        }
     }
-    if (big != noLeaf) {
-      swap(nums, big, noLeaf);
-      if (big <= m) {
-        buildMaxHeap(nums, big);
-      }
+
+
+    public int add( int val ) {
+        if (count < k) {
+            heap[count++] = val;
+            for (int i = count / 2; i >= 0; i--) {
+                buildMinHeap(heap, i);
+            }
+        } else if (val > heap[0]) {
+            heap[0] = val;
+            buildMinHeap(heap, 0);
+        }
+        return heap[0];
     }
-  }
-
-
-  public int add(int val) {
-
-
-
-
-    return nums[k - 1];
-
-  }
+   //todo 效率不行
+    public static void main( String[] args ) {
+        Solution_703 h = new Solution_703(3, new int[]{4,5,8,2});
+        System.out.println(h.add(3));
+        System.out.println(h.add(5));
+        System.out.println(h.add(10));
+        System.out.println(h.add(9));
+        System.out.println(h.add(4));
+    }
 }
