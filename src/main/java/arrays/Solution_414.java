7cd0033c0e2b3eeb775c4c9219c9d0eb2feb36bc
zhouxi <fancy9044@gmail.com>
2020-01-03 13:23:01 +0800

    414

--- src/main/java/arrays/Solution_414.java
+++ src/main/java/arrays/Solution_414.java
@@ -8,40 +8,43 @@ package arrays;
  */
 public class Solution_414 {
 
-    public int thirdMax( int[] nums ) {
-        int one = Integer.MIN_VALUE;
-        int two = one;
-        int three = two;
-        int tmp = 0;
-        for (int c : nums) {
-            if (c >= one) {
-                if (c == one) {
-                    continue;
-                }
-                tmp = one;
-                one = c;
-                three = two;
-                two = tmp;
-            } else if (c >= two) {
-                if (c == two) {
-                    continue;
-                }
-                tmp = two;
-                two = c;
-                three = tmp;
-            } else if (c >= three) {
-                if (c == three) {
-                    continue;
-                }
-                three = c;
-            }
-        }
 
-        return two > three && nums.length > 2 ? three : one;
+  public int thirdMax(int[] nums) {
+    boolean flag = false;
+    int[] sub = {Integer.MIN_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE};
+    for (int c : nums) {
+      if (c == Integer.MIN_VALUE) {
+        flag = true;
+      }
+      if (c == sub[0] || c == sub[1] || c == sub[2]) {
+        continue;
+      }
+      if (c > sub[0]) {
+        swap(sub, 1, 2);
+        swap(sub, 0, 1);
+        sub[0] = c;
+      } else if (c > sub[1]) {
+        swap(sub, 1, 2);
+        sub[1] = c;
+      } else if (c > sub[2]) {
+        sub[2] = c;
+      }
     }
+    if (flag) {
+      return sub[1] > sub[2] ? sub[2] : sub[0];
+    }
+    return sub[2] == Integer.MIN_VALUE ? sub[0] : sub[2];
 
-    public static void main( String[] args ) {
-        int[] nums = {1, 5, 5};
-        System.out.println(new Solution_414().thirdMax(nums));
-    }
+  }
+
+  private void swap(int[] array, int i, int j) {
+    int tmp = array[i];
+    array[i] = array[j];
+    array[j] = tmp;
+  }
+
+  public static void main(String[] args) {
+    int[] nums = {1, 1, 1};
+    System.out.println(new Solution_414().thirdMax(nums));
+  }
 }
